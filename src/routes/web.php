852f5f368e014e762acb2b7e95a95469e145c98c
naganuma <naganuma.tsutomu@kk-protect.co.jp>
2023-11-14 18:06:14 +0900

    update

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+Route::get('/folders/{id}/tasks', 'App\Http\Controllers\TaskController@index')->name('tasks.index');
